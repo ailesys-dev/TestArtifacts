ae7d4c4774872439ee30a27f5af5c6c63ecda298
roonawa <roonawa@ailesys.co.jp>
2025-12-08 16:57:34 +0900

    add: 中止ボタンのコード例追加

---
+++ 共通機能/コード例/検索結果エリア/中止ボタン.ts
@@ -0,0 +1,21 @@
+/** 中止ボタンのOnClickイベント */
+const OnClickCancelButton = () => {
+    // 表示順コンボボックスの選択状態を初期化する。
+    dispOrderComboBox.value = "0";
+    // 検索結果エリアの一覧データをクリアする。
+    tableItems_TblGakuseki.value = [];
+    // 一覧データ読込み前かどうか。各種ボタンの非活性などに関わる。
+    isBeforeLoadingTableItems_TblGakuseki.value = true;
+    // 全クリアボタンがある画面の場合はこれで検索条件をクリアする。
+    OnClickSearchConditionClearButton();
+    // 全クリアボタンがない場合は、各値の初期化を行う。
+    searchCondition_TblGakuseki.obFlag.value = null;
+};
+
+// 配置例
+        <section id="content02">
+            <div class="aladdbutton">
+                <AlFileOutputButton @click="OnClickFileOutputButton" />
+                <AlCancelButton @click="OnClickCancelButton" />
+            </div>
+        </section>
